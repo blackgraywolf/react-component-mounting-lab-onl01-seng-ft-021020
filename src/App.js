3e30c4579851a826666f331130bf8c0b057c486e
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-06-19 18:11:53 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 import Timer from './Timer'
 
 class App extends Component {
-
+ constructor(){
   //no props being used here, so we can use the shorthand declaration of state
+    super();
   state = {
     timerIDs: []
   }
-
+}
 
   //Your code here:
 
